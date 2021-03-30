e9950a9da4db5e356c976194b07d455234bbb0d8
the-halfbloodprince <ammanishkumardas@gmail.com>
2021-03-30 16:41:20 +0530

    Ini

--- src/script.js
+++ src/script.js
@@ -1,7 +1,12 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
+import './style.css';
+import * as THREE from 'three';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'dat.gui';
+
+const textureLoader = new THREE.TextureLoader()
+const texture = textureLoader.load('/textures/texture.jpg')
+const height = textureLoader.load('/textures/height.png')
+const alpha = textureLoader.load('/textures/alpha.jpg')
 
 // Debug
 const gui = new dat.GUI()
@@ -13,45 +18,62 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
-
-// Materials
-
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const geometry = new THREE.PlaneBufferGeometry(3, 3, 64, 64)
+const material = new THREE.MeshStandardMaterial({
+    color: 'gray',
+    map: texture,
+    displacementMap: height,
+    displacementScale: 1,
+    alphaMap: alpha,
+    transparent: true,
+    depthTest: false
+})
 
 // Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+const plane = new THREE.Mesh(geometry, material)
+scene.add(plane)
+plane.rotation.x = 181
+gui.add(plane.rotation, 'x').min(0).max(360)
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
+const pointLight = new THREE.PointLight('#7e28ff', 3)
 pointLight.position.x = 2
 pointLight.position.y = 3
 pointLight.position.z = 4
 scene.add(pointLight)
 
+gui.add(pointLight.position, 'x').min(-20).max(20)
+gui.add(pointLight.position, 'y').min(-20).max(20)
+gui.add(pointLight.position, 'z').min(-20).max(20)
+
+const lightColor = { color : '#00f'}
+gui.addColor(lightColor, 'color').onChange(()=>{
+    pointLight.color.set(lightColor.color)
+})
+
+
+
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
+    width: window.innerWidth*0.7,
     height: window.innerHeight
 }
 
 window.addEventListener('resize', () =>
 {
     // Update sizes
-    sizes.width = window.innerWidth
+    sizes.width = window.innerWidth*0.5
     sizes.height = window.innerHeight
 
     // Update camera
-    camera.aspect = sizes.width / sizes.height
+    camera.aspect = (sizes.width*0.5) / sizes.height
     camera.updateProjectionMatrix()
 
     // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
+    renderer.setSize(sizes.width*0.5, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
@@ -62,8 +84,12 @@ window.addEventListener('resize', () =>
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 0
 camera.position.y = 0
-camera.position.z = 2
+camera.position.z = 3
 scene.add(camera)
+
+gui.add(plane.rotation, 'z').min(0).max(360)
+gui.add(plane.rotation, 'y').min(0).max(360)
+gui.add(plane.rotation, 'x').min(0).max(360)
 
 // Controls
 // const controls = new OrbitControls(camera, canvas)
@@ -72,8 +98,9 @@ scene.add(camera)
 /**
  * Renderer
  */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+ const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -81,16 +108,30 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Animate
  */
+let mouseY = 0
+
+function animateTerrain(ev) {
+    mouseY = ev.clientY
+}
+
+document.addEventListener('mousemove', animateTerrain)
+
 
 const clock = new THREE.Clock()
 
 const tick = () =>
 {
+   
+    plane.rotation.z+=0.01
+    plane.material.displacementScale = .3 + mouseY*0.002
 
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+    /* sphere.rotation.y = .5 * elapsedTime
+    sphere.rotation.y += .05 * (targetY - sphere.rotation.y)
+    sphere.rotation.x += .05 * (targetX - sphere.rotation.x)
+    sphere.position.z += .005 * (targetY - sphere.rotation.x) */
 
     // Update Orbital Controls
     // controls.update()
@@ -103,3 +144,14 @@ const tick = () =>
 }
 
 tick()
+
+
+const typed = new Typed('.type', {
+    strings: ['Terrains', 'Maps', 'Contours'],
+    smartBackspace: true,
+    typeSpeed: 100,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: false,
+    backDelay: 4000
+})
